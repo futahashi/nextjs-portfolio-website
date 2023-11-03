d624b2d7e3c8b9db00defb8faa6a5fd7c6045f18
Futahashi Hisatomo <futahashi@gmail.com>
2023-11-03 20:06:48 +0900

    updte

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -9,44 +9,16 @@ import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
-  // const [emailSubmitted, setEmailSubmitted] = useState(false);
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   const data = {
-  //     email: e.target.email.value,
-  //     subject: e.target.subject.value,
-  //     message: e.target.message.value,
-  //   };
-  //   const JSONdata = JSON.stringify(data);
-  //   // const endpoint = "/api/send";
-
-  //   // Form the request for sending data to the server.
-  //   const options = {
-  //     // The method is POST because we are sending data.
-  //     method: "POST",
-  //     // Tell the server we're sending JSON.
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     // Body of the request is the JSON data we created above.
-  //     body: JSONdata,
-  //   };
-
-  //   // const response = await fetch(endpoint, options);
-  //   const resData = await response.json();
-
-  //   if (response.status === 200) {
-  //     console.log("Message sent.");
-  //     setEmailSubmitted(true);
-  //   }
-  // };
-
   return (
-    <section
-      id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
-    >
+    <section className="text-white" id="contact">
+    {/* // <section
+    //   id="contact"
+    //   className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+    // > */}
+      <div className="md:grid md:grid-cols-1 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+
+
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
       <h2 className="text-4xl font-bold text-white mb-4">Contact 🤝</h2>
@@ -80,6 +52,8 @@ const EmailSection = () => {
             <Image src={InstagramIcon} alt="Instagram Icon" />
           </Link>
         </div>
+      </div>
+      </div>
       </div>
     </section>
   );
